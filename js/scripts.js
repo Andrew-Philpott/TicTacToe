4b0153fe339bf578d0336758f827441843326fb6
Andrew Philpott and Sasa Schwartz <sasa@>
2020-01-29 09:17:56 -0800

    add logic to print board, setMark, getMark, getSpace, getCoordinates, and getSymbol

---
+++ js/scripts.js
@@ -0,0 +1,61 @@
+class Player {
+  constructor(symbol) {
+    this.symbol = symbol;
+  }
+
+  getSymbol() {
+    return this.symbol;
+  }
+}
+
+class Space {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.mark = 0;
+  }
+  setMark(playerMark) {
+    this.mark = playerMark;
+  }
+  getMark() {
+    return this.mark;
+  }
+  getCoordinates() {
+    return [this.x, this.y];
+  } 
+}
+
+class Board {
+  constructor(spacesArray) {
+    this.board = spacesArray;
+  };
+
+  getSpace(x,y) {
+    return this.board[x][y];
+  }
+
+  print() {
+    for(let row = 0; row < 3; row++) {
+      for(let col = 0; col < 3; col++) {
+        console.log(this.board[row][col].getCoordinates() + " " + this.board[row][col].getMark());
+      }
+    }
+  }
+}
+
+
+
+$(document).ready(function(){
+  let spacesArray = [[],[],[]];
+  for(let row = 0; row < 3; row++) {
+    for(let col = 0; col < 3; col++) {
+      let newSpace = new Space(row, col);
+      spacesArray[row][col] = newSpace;
+    }
+  }
+  let board = new Board(spacesArray);
+  board.print();
+  let player = new Player("X");
+  board.getSpace(0,0).setMark(player.getSymbol());
+  board.print();
+})
